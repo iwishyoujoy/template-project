793430f917335b1ab615846ceacbb0731eaa6391
dasha <dasha@skv.name>
2024-02-22 18:46:35 +0300

    added example of usage for selector, setter and api query

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,35 @@
+'use client'
+
+import { useDispatch, useSelector } from "react-redux";
 import styles from "./page.module.css";
+import { useGetSomethingByNameQuery } from "@/redux/services/api";
+import { setId } from "@/redux/someData/reducer";
+import { someDataSelector } from "@/redux/someData/selector";
 
 /* главная страница, которая будет открываться по / */
 
 export default function Home() {
+  /* пример использования query, возвращает разные индикаторы получения информации, можно добавить скелетоны и обработку ошибок */
+  const {data, isLoading, error} = useGetSomethingByNameQuery("bla");
+
+  /* пример использования селектора */
+  const something = useSelector(someDataSelector);
+
+  if (error){
+    return <div>Oops</div>;
+  }
+
+  if (isLoading){
+    return <div>some skeleton</div>;
+  }
+
+  /* пример использования сеттера*/
+  const dispath = useDispatch();
+  dispath(setId(6));
+
   return (
     <main className={styles.main}>
-      here will be content
+      first page content here
     </main>
   );
 }
